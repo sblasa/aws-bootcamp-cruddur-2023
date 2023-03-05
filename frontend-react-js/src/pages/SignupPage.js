a8caa162a519ad6dc41edbdc8cbff2e1711e5482
Sussette Blasa <stblasa@gmail.com>
2023-03-05 01:15:51 +0000

    fixes for authorization Cognito

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -1,36 +1,126 @@
+import './SignupPage.css';
+import React from "react";
+import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import { Link } from "react-router-dom";
+
+// [TODO] Authenication
 import { Auth } from 'aws-amplify';
 
-const [cognitoErrors, setCognitoErrors] = React.useState('');
+export default function SignupPage() {
 
-const onsubmit = async (event) => {
-  event.preventDefault();
-  setCognitoErrors('')
-  try {
+  // Username is Eamil
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [errors, setErrors] = React.useState('');
+
+  const onsubmit = async (event) => {
+    event.preventDefault();
+    setErrors('')
+    console.log('username',username)
+    console.log('email',email)
+    console.log('name',name)
+    try {
       const { user } = await Auth.signUp({
         username: email,
         password: password,
         attributes: {
-            name: name,
-            email: email,
-            preferred_username: username,
+          name: name,
+          email: email,
+          preferred_username: username,
         },
         autoSignIn: { // optional - enables auto sign in after user is confirmed
-            enabled: true,
+          enabled: true,
         }
       });
       console.log(user);
       window.location.href = `/confirm?email=${email}`
-  } catch (error) {
-      console.log(error);
-      setCognitoErrors(error.message)
+    } catch (error) {
+        console.log(error);
+        setErrors(error.message)
+    }
+    return false
   }
-  return false
-}
 
-let errors;
-if (cognitoErrors){
-  errors = <div className='errors'>{cognitoErrors}</div>;
-}
+  const name_onchange = (event) => {
+    setName(event.target.value);
+  }
+  const email_onchange = (event) => {
+    setEmail(event.target.value);
+  }
+  const username_onchange = (event) => {
+    setUsername(event.target.value);
+  }
+  const password_onchange = (event) => {
+    setPassword(event.target.value);
+  }
 
-//before submit component
-{errors}
+  let el_errors;
+  if (errors){
+    el_errors = <div className='errors'>{errors}</div>;
+  }
+
+  return (
+    <article className='signup-article'>
+      <div className='signup-info'>
+        <Logo className='logo' />
+      </div>
+      <div className='signup-wrapper'>
+        <form 
+          className='signup_form'
+          onSubmit={onsubmit}
+        >
+          <h2>Sign up to create a Cruddur account</h2>
+          <div className='fields'>
+            <div className='field text_field name'>
+              <label>Name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={name_onchange} 
+              />
+            </div>
+
+            <div className='field text_field email'>
+              <label>Email</label>
+              <input
+                type="text"
+                value={email}
+                onChange={email_onchange} 
+              />
+            </div>
+
+            <div className='field text_field username'>
+              <label>Username</label>
+              <input
+                type="text"
+                value={username}
+                onChange={username_onchange} 
+              />
+            </div>
+
+            <div className='field text_field password'>
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={password_onchange} 
+              />
+            </div>
+          </div>
+          {el_errors}
+          <div className='submit'>
+            <button type='submit'>Sign Up</button>
+          </div>
+        </form>
+        <div className="already-have-an-account">
+          <span>
+            Already have an account?
+          </span>
+          <Link to="/signin">Sign in!</Link>
+        </div>
+      </div>
+    </article>
+  );
+}
