c0d27c5b1f1894cdecf29b961137ee00b44cabf1
Sussette Blasa <stblasa@gmail.com>
2023-03-12 02:14:25 +0000

    fix issues with react

--- frontend-react-js/src/components/DesktopNavigationLink.js
+++ frontend-react-js/src/components/DesktopNavigationLink.js
@@ -31,6 +31,9 @@ export default function DesktopNavigationLink(props) {
       case 'messages':
         return <MessagesIcon className='icon' />
         break;
+      default:  
+      return <HomeIcon className='icon' />
+        break;
     }
   }
 
