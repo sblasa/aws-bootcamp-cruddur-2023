2fd8cd8fab1ebbbd121d1084fb838b2ace4353f4
Sussette Blasa <stblasa@gmail.com>
2023-03-17 20:13:05 -0700

    remove file

--- backend-flask/db/schema.sql
+++
@@ -1,3 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-export CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
-gp env CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
