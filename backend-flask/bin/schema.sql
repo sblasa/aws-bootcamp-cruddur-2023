a6271c0d758bc85532013edfc6952673d016fb50
Sussette Blasa <stblasa@gmail.com>
2023-03-18 03:39:34 +0000

    add back cleaned up schema

---
+++ backend-flask/bin/schema.sql
@@ -0,0 +1,3 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+export CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
+gp env CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
