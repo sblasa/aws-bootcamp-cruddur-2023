cea769827723feadf0aa9c407867398c32da540a
Sussette Blasa <stblasa@gmail.com>
2023-05-06 19:00:05 +0000

    fix expiring tokens

--- frontend-react-js/src/components/ActivityForm.js
+++ frontend-react-js/src/components/ActivityForm.js
@@ -23,11 +23,9 @@ export default function ActivityForm(props) {
         method: "POST",
         headers: {
           'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          user_handle: props.user_handle.handle,
           message: message,
           ttl: ttl
         }),
--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -2,6 +2,7 @@ import './MessageForm.css';
 import React from "react";
 import process from 'process';
 import { json, useParams } from 'react-router-dom';
+import {checkAuth, getAccessToken} from '../lib/CheckAuth';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -29,7 +30,7 @@ export default function ActivityForm(props) {
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
-          'Authorization': `Bearer ${localStorage.getItem("access_token")}`,
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
