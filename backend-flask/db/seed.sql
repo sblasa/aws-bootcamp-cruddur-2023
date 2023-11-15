ed4550ea3d6201256186577f2511c11656b75681
Sussette Blasa <stblasa@gmail.com>
2023-11-15 03:54:15 +0000

    updated the wrong user

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'hello@me.com', 'andrewbrown', 'MOCK'),
-  ('altmama', 'msblasa@gmail.com', 'altmama', 'MOCK'),
+  ('itsmamahello', 'msblasa@gmail.com', 'itsmamahello', 'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK'),
   ('Sussette','msblasa@gmail.com' ,'sblasa' ,'MOCK'),
   ('Mama', 'hello@me.com', 'itsmamahello', 'MOCK'),
