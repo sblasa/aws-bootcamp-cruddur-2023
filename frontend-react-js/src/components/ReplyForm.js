abdaedf0afe2b09bb9147d85a1c07d54683997f7
Sussette Blasa <stblasa@gmail.com>
2024-01-03 05:14:35 +0000

    put in setErrors to clear errors

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -19,6 +19,7 @@ export default function ReplyForm(props) {
 
   const onsubmit = async (event) => {
     event.preventDefault();
+    setErrors('');
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`;
     const payload_data = {
       activity_uuid: props.activity.uuid,
