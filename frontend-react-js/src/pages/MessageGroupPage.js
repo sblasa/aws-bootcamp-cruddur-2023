eccc2ce4e6d6c72496e68d8dbb4b1fa06e16945f
sblasa <stblasa@gmail.com>
2024-02-04 12:39:52 -0800

    fix issue displaying messagegroups missing function

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -1,46 +1,51 @@
-import './MessageGroupPage.css';
-import React from "react";
-import { useParams } from 'react-router-dom';
+import './MessageGroupPage.css'
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
-import {checkAuth} from 'lib/CheckAuth';
-import {get} from 'lib/Requests';
+import { checkAuth } from 'lib/CheckAuth'
+import { get } from 'lib/Requests'
 
-import DesktopNavigation  from 'components/DesktopNavigation';
-import MessageGroupFeed from 'components/MessageGroupFeed';
-import MessagesFeed from 'components/MessageFeed';
-import MessagesForm from 'components/MessageForm';
+import DesktopNavigation from 'components/DesktopNavigation'
+import MessageGroupFeed from 'components/MessageGroupFeed'
+import MessagesFeed from 'components/MessageFeed'
+import MessagesForm from 'components/MessageForm'
 
 export default function MessageGroupPage() {
-  const [messageGroups, setMessageGroups] = React.useState([]);
-  const [messages, setMessages] = React.useState([]);
-  const [popped, setPopped] = React.useState([]);
-  const [user, setUser] = React.useState(null);
-  const dataFetchedRef = React.useRef(false);
-  const params = useParams();
+  const [messageGroups, setMessageGroups] = React.useState([])
+  const [messages, setMessages] = React.useState([])
+  const [popped, setPopped] = React.useState([])
+  const [user, setUser] = React.useState(null)
+  const dataFetchedRef = React.useRef(false)
+  const params = useParams()
 
   const loadMessageGroupsData = async () => {
-
-      const url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
-      get(url, null, function(data){
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
+    get(url, {
+      auth: true,
+      success: function (data) {
         setMessageGroups(data)
-      })
-  };  
+      },
+    })
+  }
 
   const loadMessageGroupData = async () => {
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
-    get(url, null, function(data){
-      setMessages(data)
+    get(url, {
+      auth: true,
+      success: function (data) {
+        setMessages(data)
+      },
     })
-  };  
+  }
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     //prevents double call
-    if (dataFetchedRef.current) return;
-    dataFetchedRef.current = true;
+    if (dataFetchedRef.current) return
+    dataFetchedRef.current = true
 
-    loadMessageGroupsData();
-    loadMessageGroupData();
-    checkAuth(setUser);
+    loadMessageGroupsData()
+    loadMessageGroupData()
+    checkAuth(setUser)
   }, [])
   return (
     <article>
@@ -53,5 +58,5 @@ export default function MessageGroupPage() {
         <MessagesForm setMessages={setMessages} />
       </div>
     </article>
-  );
+  )
 }
